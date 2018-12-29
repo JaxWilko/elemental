e86566b094b89cb36abb294f2829132b3cfc9307
Jack Wilkinson <jack@plekko.com>
2018-12-29 01:34:54 +0000

    Updated working index

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
 require __DIR__ . '/vendor/autoload.php';
 
 echo element('div')
+    ->before(
+        element('div')
+            ->id('header')
+            ->addChild(element('span')->text('hello world'))
+    )
+    ->after(
+        element('div')
+            ->id('footer')
+            ->addChild(element('span')->text('goodbye world'))
+    )
     ->addClass('container')
     ->addChild(element('span')->text('this is a hat'))
     ->addChild(
